61412fde3f49a2f73db797f159077408dd32902e
ChanhoHwang <ChanhoHwang>
2021-10-06 22:36:03 +0900

    Initial Commit

---
+++ SeSSAC_EmotionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSSAC_EmotionDiary
+//
+//  Created by ChanhoHwang on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
